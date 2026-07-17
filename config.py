bdf6a3d975d7f6c823bc7d850507a3bb644cc89e
Haris Arif <harisarifardiansyah@students.amikom.ac.id>
2026-07-17 23:10:09 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+class Config:
+    """
+    Konfigurasi aplikasi Flask
+    """
+
+    # Konfigurasi Database MySQL
+    HOST = "4zddb8.h.filess.io"
+    PORT = 61000
+    USER = "db_academic_oldermudat"
+    PASSWORD = "f72d92dc0ea847de31f1bac2fb31f9bd05323932"
+    DATABASE = "db_academic_oldermudat"
+
+    # Secret Key Flask
+    SECRET_KEY = "praktikum-flask-2026"
+
+    # Konfigurasi Flask
+    DEBUG = True
