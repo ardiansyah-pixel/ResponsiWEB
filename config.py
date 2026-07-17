8c9b0223669afd9ba4e21f29b217bd276680a4e1
Haris Arif <harisarifardiansyah@students.amikom.ac.id>
2026-07-17 23:26:29 +0700

    Initial commit

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config:
     """
 
     # Konfigurasi Database MySQL
-    HOST = "4zddb8.h.filess.io"
-    PORT = 61000
-    USER = "db_academic_oldermudat"
-    PASSWORD = "f72d92dc0ea847de31f1bac2fb31f9bd05323932"
-    DATABASE = "db_academic_oldermudat"
+    hostname = "4zddb8.h.filess.io"
+    database = "db_academic_oldermudat"
+    port = "61000"
+    username = "db_academic_oldermudat"
+    password = "f72d92dc0ea847de31f1bac2fb31f9bd05323932"
 
     # Secret Key Flask
     SECRET_KEY = "praktikum-flask-2026"
